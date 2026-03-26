e6bf09b7a5590d374962a4af755a5fefabc599cb
Santiago Moreno <149446341+DarkAscen@users.noreply.github.com>
2026-03-26 16:13:14 -0300

    Update compra.js

--- scripts/compra.js
+++ scripts/compra.js
@@ -1,39 +1,89 @@
 const tabla = document.querySelector("table tbody")
 const botonComprar = document.querySelector("button#pagar")
 const totalCompra = document.querySelector("td.precioTotal")
-const botonEliminar = document.querySelector("th.eliminar")
+
 
 function devolverCarrito() {
     return JSON.parse(localStorage.getItem("miCarrito"))
 }
 
+
 const carrito = devolverCarrito()
 
-function filaDeProducto(peluche) {
+
+function filaDePeluche(peluche) {
     return `<tr>
-                <td><img src="${peluche.imagen}" alt="${peluche.nombre}" class="imagenCheck"></td>
+                <td><img src=".${peluche.imagen}" alt="${peluche.nombre}" class="imagenCheck"></td>
                 <td>${peluche.nombre}</td>
-                <td>$${peluche.valor} USD</td>
-                <td>ELIMINAR PRODUCTO</td>
+                <td>${peluche.cantidad}</td>
+                <td>$ ${peluche.valor} USD</td>
+                <td class="eliminar" id=${peluche.id}><span id="texto-eliminar">ELIMINAR</span></td>
             </tr>`
-    
 }
 
-if (carrito) {
-    carrito.forEach((peluche)=> {
-        tabla.innerHTML += filaDeProducto(peluche);
-    })
+
+function filaVacia() {
+    return `<tr>
+                <td>Fila</td>
+                <td>vacia</td>
+                <td>de</td>
+                <td>producto</td>
+            </tr>`
 }
 
-function totalCarrito (){
+
+function cargarCarrito() {
+    if (carrito) {
+        tabla.innerHTML = ""
+        carrito.forEach((peluche)=> {
+            tabla.innerHTML += filaDePeluche(peluche)
+        })
+        clickEliminar()
+    } else {
+        tabla.innerHTML = filaVacia()
+    }
 }
+cargarCarrito()
+
 
 botonComprar.addEventListener("click", ()=> {
-    finalizarCompra()
-    localStorage.removeItem("miCarrito")
-    carrito.length = 0
-    botonComprar.setAttribute("disabled", "true")
+    Swal.fire({
+        icon: 'question',
+        title: 'Confirmar compra',
+        text: 'El valor de la compra es de $' + totalCarrito() + ' USD, ¿Desea realizar la compra?',
+        showCancelButton: true,
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No'
+    }).then((respuesta)=> {
+        if(respuesta.isConfirmed) {
+            localStorage.removeItem("miCarrito")
+            carrito.length = 0
+            botonComprar.setAttribute("disabled", "true")
+            finalizarCompra()
+            cargarCarrito()
+            mostrarTotal()
+        } 
+    })
 })
+
+
+function clickEliminar() {
+    const botonesEliminar = document.querySelectorAll("td.eliminar")
+    if (botonesEliminar.length > 0) {
+        botonesEliminar.forEach((botonEliminar)=> {
+            botonEliminar.addEventListener("click", ()=> eliminarPeluche(botonEliminar.id))
+        })
+    }
+}
+
+
+function eliminarPeluche(pelucheId) {
+    const indice = carrito.findIndex((peluche)=> peluche.id === parseInt(pelucheId))
+    carrito[indice].cantidad === 1 ? carrito.splice(indice, 1) : carrito[indice].cantidad--
+    cargarCarrito()
+    mostrarTotal()
+}
+
 
 function finalizarCompra() {
     Swal.fire({
@@ -44,12 +94,21 @@ function finalizarCompra() {
     })
 }
 
+
 function totalCarrito() {
-    let total = 0;
-    carrito.forEach((peluche) => {
-        total += peluche.valor;
-    });
-    return total;
+    if (carrito.length > 0) {
+        let total = 0;
+        carrito.forEach((peluche) => {
+            total += peluche.valor * peluche.cantidad;
+        });
+        return total;
+    } else {
+        return 0;
+    }
 }
 
-totalCompra.textContent = totalCarrito() + " USD";
+
+function mostrarTotal() {
+    totalCompra.textContent =  "$ " + totalCarrito() + " USD";
+}
+mostrarTotal()
