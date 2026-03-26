5d45792ac1268a83ff512af21fadaa0408c60315
Santiago Moreno <149446341+DarkAscen@users.noreply.github.com>
2026-03-26 15:42:47 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -64,7 +64,7 @@ pagarCarrito.addEventListener("mousemove", ()=> {
 })
 
 pagarCarrito.addEventListener("click", ()=> {
-    carrito.length > 0 ? location.href = "./paginas/checkout.html" : notificar('El carrito se encuentra vacio.', 'darkorange');
+    carrito.length > 0 ? location.href = "paginas/checkout.html" : notificar('El carrito se encuentra vacio.', 'darkorange');
 })
 
 function botonesComprar () {
