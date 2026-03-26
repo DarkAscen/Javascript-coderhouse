cee520737907c96252ab809afe16b1faa20cbad9
Santiago Moreno <149446341+DarkAscen@users.noreply.github.com>
2026-03-26 16:14:39 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,19 @@ const contenedor = document.getElementById("contenedor")
 const pagarCarrito = document.querySelector("img#carrito")
 const botonVolver = document.querySelector("button#volver")
 const inputBuscar = document.querySelector("input.buscador")
+const recibirPeluches = "scripts/peluchesDisponibles.json"
+
 
 function recuperarCarrito() {
     return JSON.parse(localStorage.getItem("miCarrito")) ?? []
 }
 
+
 const carrito = recuperarCarrito()
 
-let peluchesDisponibles = [{nombre: "OSO POLAR", id: 1, valor: 46, descripcion: "Peluche de un oso polar", imagen: "./imagenes/osoPolar.webp"},
-    {nombre: "PINGUINO", id: 2, valor: 32, descripcion: "Peluche de un pinguino", imagen: "./imagenes/pinguino.webp"},
-    {nombre: "FOCA", id: 3, valor: 18, descripcion: "Peluche de una foca", imagen: "./imagenes/foca.webp"},
-    {nombre: "ORCA", id: 4, valor: 22, descripcion: "Peluche de orca", imagen: "./imagenes/orca.jpg"},
-    {nombre: "BALLENA AZUL", id: 5, valor: 36, descripcion: "Peluche de ballena azul", imagen: "./imagenes/ballenaAzul.jpg"},
-    {nombre: "PEZ", id: 6, valor: 16, descripcion: "Peluche de pez de la Antartida", imagen: "./imagenes/pez.jpeg"}];
+
+const peluchesDisponibles = [];
+
 
 inputBuscar.addEventListener("keypress", (e)=> {
     if (e.key === "Enter" && inputBuscar.value.trim() !== "") {
@@ -23,18 +23,33 @@ inputBuscar.addEventListener("keypress", (e)=> {
     }
 })
 
+
 inputBuscar.addEventListener("input", ()=> {
     inputBuscar.value.trim() === "" && cargarPeluches(peluchesDisponibles);
 })
+
+
+function peluchesJson() {
+    fetch(recibirPeluches)
+    .then((response)=> response.json())
+    .then((data)=> peluchesDisponibles.push(...data))
+    .then(()=> cargarPeluches(peluchesDisponibles))
+    .catch((error)=> contenedor.innerHTML = cartaError())
+}
+
+
+peluchesJson()
+
 
 function cartaPeluche (peluche) {
     return `<div class="carta">
                 <div><img src="${peluche.imagen}" class="imagen"></div>
                 <div class="cartaNombre"><p>${peluche.nombre}</p></div>
                 <div class="cartaPrecio"><p>$${peluche.valor} USD</p></div>
-                <button id="${peluche.id}" class="button button-outline button-add botonesCheck" title="Agregar al carrito">COMPRAR</button>
+                <button id="${peluche.id}" class="button button-outline button-add" title="Agregar al carrito">COMPRAR</button>
             </div>`
 }
+
 
 function cartaError () {
     return `<div class="card">
@@ -42,9 +57,22 @@ function cartaError () {
             </div>`
 }
 
+
+function retornoPelucheNuevo(pelucheSeleccionado) {
+    return pelucheNuevo = {
+        id: pelucheSeleccionado.id,
+        imagen: pelucheSeleccionado.imagen,
+        nombre: pelucheSeleccionado.nombre,
+        cantidad: 1,
+        valor: pelucheSeleccionado.valor,
+    }
+}
+
+
 function cargarPeluches (array) {
     if (array.length > 0) {
         contenedor.innerHTML = "";
+
 
         array.forEach((peluche)=> {
             contenedor.innerHTML += cartaPeluche(peluche)
@@ -55,7 +83,6 @@ function cargarPeluches (array) {
     }
 }
 
-cargarPeluches(peluchesDisponibles)
 
 pagarCarrito.addEventListener("mousemove", ()=> {
     if (carrito.length > 0) {
@@ -63,21 +90,30 @@ pagarCarrito.addEventListener("mousemove", ()=> {
     }
 })
 
+
 pagarCarrito.addEventListener("click", ()=> {
-    carrito.length > 0 ? location.href = "./paginas/checkout.html" : notificar('El carrito se encuentra vacio.', 'darkorange');
+    carrito.length > 0 ? location.href = "../paginas/checkout.html" : notificar('El carrito se encuentra vacio.', 'darkorange');
 })
+
 
 function botonesComprar () {
     const botonesComprar = document.querySelectorAll("button.button-add");
     for (let boton of botonesComprar) {
         boton.addEventListener("click", ()=> {
             const pelucheSeleccionado = peluchesDisponibles.find((peluche)=> peluche.id === parseInt(boton.id));
-            carrito.push(pelucheSeleccionado);
+            const enElCarrito = carrito.findIndex((peluche)=> peluche.id === pelucheSeleccionado.id)
+            if (enElCarrito === -1) {
+                const pelucheNuevo = retornoPelucheNuevo(pelucheSeleccionado)
+                carrito.push(pelucheNuevo);
+            } else {
+                carrito[enElCarrito].cantidad++;
+            }
             localStorage.setItem("miCarrito", JSON.stringify(carrito))
             notificar(`${pelucheSeleccionado.nombre} se agregó al carrito.`, 'green')
         })
     }
 }
+
 
 function notificar(mensaje, estilo) {
     Toastify({
